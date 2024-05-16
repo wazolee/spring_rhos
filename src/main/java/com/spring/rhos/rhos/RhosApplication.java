3b389a145aad5ff0de79ee05442d086af2be8e3d
ZV <wazolee@gmail.com>
2024-05-16 16:25:39 +0200

    first commit

---
+++ src/main/java/com/spring/rhos/rhos/RhosApplication.java
@@ -0,0 +1,13 @@
+package com.spring.rhos.rhos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/rhos/rhos/RhosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.rhos.rhos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
